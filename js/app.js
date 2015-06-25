cee5419c58502da9e93ae3693470a4990cf10c5a
xxIFxx <ksa.if.reg@mail.ru>
2015-06-25 20:38:30 +0300

    Signed-off-by: xxIFxx <ksa.if.reg@mail.ru> добавили чтение с сайта и вывод в браузер картинок

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
 // JavaScript Document
 var main = function () {
  "use strict";
- console.log("Hello world");
+ // на самом деле это всего одна строка,
+ // но я разделил ее на несколько
+ // для улучшения восприятия
+ var url = "http://api.flickr.com/services/feeds/photos_public.gne?" +
+ "tags=dogs&format=json&jsoncallback=?";
+ $.getJSON(url, function (flickrResponse) {
+  flickrResponse.items.forEach(function(photo) {
+   // создаем новый элемент jQuery для помещения в него изображения
+   var $img = $("<img>");
+   // помещаем в атрибут URL, хранящийся в ответе Flickr
+   $img.attr("src", photo.media.m);
+   // прикрепляем тег img к элементу main.photos
+   $("main .photos").append($img);   
+  });
+ });
 };
 $(document).ready(main);
